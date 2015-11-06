006b8cac50ba5c1457c5aa42bef29d0be20517f5
Ajay Lalwani <lalwaniajay14@gmail.com>
2015-11-06 15:08:11 -0500

    Adding basic metadata to the cards.

---
+++ BlogReader/BlogReader/Helpers/UIFont+Style.swift
@@ -0,0 +1,21 @@
+//
+//  UIFont+Style.swift
+//  BlogReader
+//
+//  Created by Ajay Lalwani on 11/6/15.
+//  Copyright © 2015 The Red Voice. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    class var fontForBlogHeader: UIFont {
+        return UIFont(name: "Avenir-Book", size: 50)!;
+    }
+    
+    class var fontForBlogText: UIFont {
+        return UIFont(name: "Avenir-Light", size: 20)!;
+    }
+    
+}
--- BlogReader/BlogReader/Models/ScrollViewHelper.swift
+++ BlogReader/BlogReader/Models/ScrollViewHelper.swift
@@ -86,11 +86,43 @@ class ScrollViewHelper: UIView {
         // Add Image
         let imageView = UIImageView(frame: CGRectMake(CGFloat(PADDING), CGFloat(PADDING), CGRectGetWidth(frame) - CGFloat(2 * PADDING), CGRectGetHeight(frame) - CGFloat(2 * PADDING)))
         imageView.backgroundColor = UIColor.whiteColor();
-        //imageView.image = UIImage(named: "Heena");
+        imageView.image = UIImage(named: "Heena");
         imageView.contentMode = UIViewContentMode.ScaleAspectFill;
         imageView.clipsToBounds = true;
         
+        
+        // Overlay
+        let overlayView = UIView(frame: CGRectMake(0, CGRectGetHeight(contentView.bounds) - 200 , CGRectGetWidth(contentView.bounds), 200));
+        overlayView.backgroundColor = UIColor.blackColor();
+        overlayView.alpha = 0.3;
+        
+        // MetaView
+        let metaView = UIView(frame: overlayView.frame);
+        
+        // Title
+        let lblTitle = UILabel(frame: CGRectMake(0,10, CGRectGetWidth(metaView.bounds), 50));
+        lblTitle.text = "The feels of Istanbul";
+        lblTitle.textColor = UIColor.whiteColor();
+        lblTitle.textAlignment = NSTextAlignment.Center;
+        lblTitle.font = UIFont.fontForBlogHeader;
+    
+        // Partial Content
+        let lblContent = UILabel(frame: CGRectMake(30,70, CGRectGetWidth(metaView.bounds) - 60, 90));
+        
+        //300 Characters maximum
+        lblContent.text = "\"The moment’s one of a kind, 39ft above the ground, smack right in the middle of the sky, there’s a picture so perfect that it breaks your heart, in a good way. Seeing the twilight blue horizons blazing in the background, disappearing into a sky full of stars, shining like the perfect little trinkets ...";
+        
+        lblContent.textColor = UIColor.whiteColor();
+        lblContent.font = UIFont.fontForBlogText;
+        lblContent.numberOfLines = 4;
+        lblContent.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+        
+        metaView.addSubview(lblTitle);
+        metaView.addSubview(lblContent);
+        
         contentView.addSubview(imageView);
+        contentView.addSubview(overlayView);
+        contentView.addSubview(metaView);
         
         return contentView;
 
